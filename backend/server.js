0659b65be7391d3c72192d4487a8cb2eaefa1dd7
09singh <singhsanket745@gmail.com>
2026-05-11 11:17:58 +0530

    cors error

--- backend/server.js
+++ backend/server.js
@@ -8,20 +8,15 @@ import dotenv from "dotenv";
 // import aichat from "./routes/aichat.js";
 dotenv.config();
 const app = express();
-app.use(cors());
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://bizpilot-solup.netlify.app/");
-    res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
 
-    if (req.method === "OPTIONS") {
-        return res.sendStatus(204);
-    }
-
-    next();
-});
+app.use(cors({
+  origin: "https://bizpilot-solup.netlify.app",
+  methods: ["GET","POST","PUT","DELETE"],
+  credentials: true
+}));
 
 app.use(express.json());
+
 
 //mongos
 mongoose.connect(process.env.MONGO_URI)
