6cbc9adc6c7f4ba03d3b516beea2d4ef6afc45bb
09singh <singhsanket745@gmail.com>
2026-05-11 13:59:33 +0530

    solve error

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: "https://bizpilot-solup.netlify.app",
+  origin: "https://flowbit-solup.netlify.app",
   methods: ["GET","POST","PUT","DELETE"],
   credentials: true
 }));
